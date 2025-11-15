cf9c3f4fd44b3fc6b6364c45c6e4364e06f47c55
Temporary User <temp@example.com>
2025-11-15 18:03:08 +0000

    Incremental changes

--- api/gen/go/v1alpha/ext.go
+++ api/gen/go/v1alpha/ext.go
@@ -1,3 +1,18 @@
 package spherev1alpha
 
-// Package spherev1alpha contains the generated protobuf code for the Sphere API v1alpha.
+import (
+	"gorgonia.org/tensor"
+)
+
+// Dense will convert a remotely defined tensor to a local gorgonia dense tensor.
+func (t *Tensor) Dense() *tensor.Dense {
+	shape := []int{}
+	for _, i := range t.GetShape() {
+		shape = append(shape, int(i))
+	}
+	tens := tensor.New(tensor.WithShape(shape...), tensor.WithBacking(t.Data))
+	return tens
+}
+
+// IsEnvWrapper exports the interface for env wrappers.
+type IsEnvWrapper = isEnvWrapper_Wrapper
